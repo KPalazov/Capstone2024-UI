b213169d576193adf7a9762fa70378372d71cf2b
KPalazov <105868237+KPalazov@users.noreply.github.com>
2024-03-25 08:35:38 -0400

    init commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,28 @@
-import React, { Fragment } from "react";
-import { Helmet } from "react-helmet";
-import "@mdi/font/css/materialdesignicons.min.css";
-import { Link } from "react-router-dom";
-
-import logo from "../assets/img/logo.png";
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <Fragment>
-    <Helmet>
-      <title>Quiz App - Home</title>{" "}
-    </Helmet>
-    <div id="home">
-      <section>
-        <div style={{ textAlign: "center" }}>
-          <img id="mainLogo" src={logo} alt="Data Defenders" />
+    <Fragment>
+        <Helmet><title>Home - Quiz App</title></Helmet>
+        <div id="home">
+            <section>
+                <div style={{ textAlign: 'center' }}>
+                    <span className="mdi mdi-cube-outline cube"></span>
+                </div>
+                <h1>Quiz App</h1>
+                <div className="play-button-container">
+                    <ul>
+                        <li><Link className="play-button" to="/play/instructions">Play</Link></li>
+                    </ul>
+                </div>
+                <div className="auth-container">
+                    <Link to="/login" className="auth-buttons" id="login-button">Login</Link>
+                    <Link to="/register" className="auth-buttons" id="signup-button">Sign up</Link>
+                </div>
+            </section>
         </div>
-        <h1>INFO10111 Cyber Security</h1>
-        <div className="play-button-container">
-          <ul>
-            <li>
-              <Link className="play-button" to="/play/instructions">
-                Play
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="auth-container">
-          <Link to="/login" className="auth-buttons" id="login-button">
-            Login
-          </Link>
-          <Link to="/register" className="auth-buttons" id="signup-button">
-            Register
-          </Link>
-        </div>
-      </section>
-    </div>
-  </Fragment>
+    </Fragment>
 );
 
 export default Home;
